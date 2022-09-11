4a1ba79cedb424ba0fea2d28a2d62f5b2859d750
Matti Österbacka <matti.osterbacka@gmail.com>
2022-09-11 13:29:14 +0300

    further time expenditure optimization tryouts

--- src/main/java/bot/Firstbot.java
+++ src/main/java/bot/Firstbot.java
@@ -14,24 +14,25 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import static java.time.Instant.now;
+
 public class Firstbot {
     public static void main(final String[] args) {
         final String timeZone = System.getenv("timezone");
         final String token = System.getenv("discordbottoken");
         final String postgresqladdress = System.getenv("postgresqladdress");
+
+        final String channelid = System.getenv("channelid");
         final int[] day = {LocalDateTime.now(ZoneId.of(timeZone)).getDayOfYear()};
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            LocalDateTime currentTime = event.getMessage().getTimestamp().atZone(ZoneId.of(timeZone)).toLocalDateTime();
+            LocalDateTime currentTime = now().atZone(ZoneId.of(timeZone)).toLocalDateTime();
             int currentDay = currentTime.getDayOfYear();
             day[0] = currentDay;
             PsqlDb db = new PsqlDb(postgresqladdress);
-            final MessageChannel channel = message.getChannel().block();
+            final MessageChannel channel  = (MessageChannel) gateway.getChannelById(Snowflake.of(channelid)).block();
             Message message1 = channel.
-                    getMessagesAfter(Snowflake.of(event.getMessage().
-                            getTimestamp().minusSeconds(84600))).
+                    getMessagesAfter(Snowflake.of(now().minusSeconds(84600))).
                     toStream().
                     filter(message2 -> message2.getTimestamp().atZone(ZoneId.of(timeZone)).
                             getDayOfYear() ==currentDay).
@@ -39,10 +40,10 @@ public class Firstbot {
                     .findFirst().get();
             String user_id = message1.getAuthor().get().getId().toString();
             String username = message1.getAuthor().get().getUsername().toString();
-            db.addFirst(user_id, username, message1.getTimestamp().atZone(ZoneId.of(timeZone)).toLocalDateTime());
+           // db.addFirst(user_id, username, message1.getTimestamp().atZone(ZoneId.of(timeZone)).toLocalDateTime());
             channel.createMessage(new MessageString().firstOutput(db, username, user_id, currentTime)).block();
+            channel.createMessage("TESTI").block();
             System.exit(0);
-        });
 
         gateway.onDisconnect().block();
     }
